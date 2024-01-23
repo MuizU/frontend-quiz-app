f5b34ca7dc440d60a97d278148cde335c657b567
muizu <muizuvais99@gmail.com>
2024-01-23 23:05:13 +0530

    added colos

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,23 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      primary: "#A729F5",
+      darkNavy: "#313E51",
+      navy: "#3B4D66",
+      greyNavy: "#626C7F",
+      skyBlue: "#ABC1E1",
+      cloudWhite: "#F4F6FA",
+      white: "#FFFFFF",
+      emerald: "#26D782",
+      rubyRed: "#EE5454",
     },
   },
   plugins: [],
-}
+};
 export default config
