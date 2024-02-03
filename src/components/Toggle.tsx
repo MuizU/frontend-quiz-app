d27e69d8e193b18cdf8671055962d23477173337
muizu <muizuvais99@gmail.com>
2024-02-03 19:33:51 +0530

    updated toggle

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,7 +1,7 @@
 import react, { ChangeEventHandler } from "react";
 
 type TToggle = {
-  value: string;
+  value: boolean;
   onChange: ChangeEventHandler<HTMLInputElement>;
 };
 const Toggle = ({ value, onChange }: TToggle) => {
@@ -9,11 +9,11 @@ const Toggle = ({ value, onChange }: TToggle) => {
     <label className="relative inline-flex items-center cursor-pointer">
       <input
         type="checkbox"
-        value={value}
+        checked={value}
         onChange={onChange}
         className="sr-only peer"
       />
-      <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full after:border-white peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600 bg-primary"></div>
+      <div className="w-11 h-6  peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full after:border-white peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600 bg-primary"></div>
     </label>
   );
 };
