8625c9a6d8f157f721351bb40a440da33d3dd074
muizu <muizuvais99@gmail.com>
2024-02-03 20:37:26 +0530

    added icons to the theme switcher

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -2,6 +2,11 @@
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 import Toggle from "./Toggle";
+import Image from "next/image";
+import moonDarkIcon from "../../assets/images/icon-moon-dark.svg";
+import moonLightIcon from "../../assets/images/icon-moon-light.svg";
+import sunDarkIcon from "../../assets/images/icon-sun-dark.svg";
+import sunLightIcon from "../../assets/images/icon-sun-light.svg";
 
 const ThemeSwitcher = (): JSX.Element | null => {
   const [mounted, setMounted] = useState<boolean>(false);
@@ -15,10 +20,22 @@ const ThemeSwitcher = (): JSX.Element | null => {
   }
 
   return (
-    <Toggle
-      value={theme === "light"}
-      onChange={() => setTheme(theme === "dark" ? "light" : "dark")}
-    />
+    <div className="flex justify-center gap-4">
+      {theme === "dark" ? (
+        <Image src={sunLightIcon} width={20} height={20} alt="sun-dark" />
+      ) : (
+        <Image src={sunDarkIcon} width={20} height={20} alt="sun-light" />
+      )}
+      <Toggle
+        value={theme === "dark"}
+        onChange={() => setTheme(theme === "light" ? "dark" : "light")}
+      />
+      {theme === "dark" ? (
+        <Image src={moonLightIcon} width={20} height={20} alt="moon-dark" />
+      ) : (
+        <Image src={moonDarkIcon} width={20} height={20} alt="moon-light" />
+      )}
+    </div>
   );
 };
 
