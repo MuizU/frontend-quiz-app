68febb1c2dcbffdd09ec9cae10e9e19e34021f48
muizu <muizuvais99@gmail.com>
2024-02-03 20:06:03 +0530

    removed script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Script from "next/script";
 export default function Home() {
   return (
     <>
-      <Script src="./dark-mode.ts" />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
       </main>
     </>
