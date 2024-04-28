d384f7ed483d8012ed19b4136f7d4d67b32c726a
muizu <muizuvais99@gmail.com>
2024-04-28 12:18:44 +0530

    Refactor SelectionButton component for consistent formatting and dynamic width

--- src/components/SelectionButton.tsx
+++ src/components/SelectionButton.tsx
@@ -1,12 +1,12 @@
-"use client"
-import React, { ReactNode } from 'react'
+"use client";
+import React, { ReactNode } from "react";
 type TButton = {
-    children: ReactNode
-}
-export default function SelectionButton({children}:TButton) {
-    return (
-        <button className='dark:bg-[#3b4d66] h-[20px] w-[300px] z-0 px-5 py-8 flex justify-start items-center gap-5 rounded-lg' >
-        {children}
-        </button>
-    )
+  children: ReactNode;
+};
+export default function SelectionButton({ children }: TButton) {
+  return (
+    <button className="dark:bg-[#3b4d66] bg-[#fff] h-[20px] w-[25vw] z-0 px-5 py-8 flex justify-start items-center gap-5 rounded-lg">
+      {children}
+    </button>
+  );
 }
