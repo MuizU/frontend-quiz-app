fdcbd6b25fd0b7eb275ede7f3aaa12aa94a85ca8
muizu <muizuvais99@gmail.com>
2024-05-01 23:19:11 +0530

    added div

--- src/app/quiz/[...slug]/page.tsx
+++ src/app/quiz/[...slug]/page.tsx
@@ -1,9 +1,7 @@
 export default function Quiz({ params }: { params: { slug: string } }) {
   return (
-          <main className="flex min-h-screen items-start justify-between px-24 py-12 text-[#313e51] dark:text-white">
-          <div className="flex flex-col gap-1">
-
-          </div>
-          </main>
-  )
+    <main className="flex min-h-screen items-start justify-between px-24 py-12 text-[#313e51] dark:text-white">
+      <div className="flex flex-col gap-1"></div>
+    </main>
+  );
 }
