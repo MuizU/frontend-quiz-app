17cee96278fa32053ba839fba67027c6f2b75df8
muizu <muizuvais99@gmail.com>
2024-05-01 20:41:00 +0530

    Revert "converted to client"
    This reverts commit 6774384587178953c61271d10c92871b71124c9b.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,10 @@
-"use client";
 import SelectionButton from "@/components/SelectionButton";
 import { promises as fs } from "fs";
 import Image from "next/image";
-import { useCallback, useEffect, useState } from "react";
 
-export default function Home() {
-  const [quizzes, setQuizzes] = useState<[] | null>();
-  const fetchQuizzes = useCallback(async () => {
-    const file = await fs.readFile(process.cwd() + "/data.json", "utf8");
-    const { quizzes } = JSON.parse(file);
-    setQuizzes(quizzes);
-  }, []);
-
-  useEffect(() => {
-    fetchQuizzes();
-  }, [fetchQuizzes]);
-
+export default async function Home() {
+  const file = await fs.readFile(process.cwd() + "/data.json", "utf8");
+  const { quizzes } = JSON.parse(file);
   return (
     <>
       <main className="flex min-h-screen items-start justify-between px-24 py-12 text-[#313e51] dark:text-white">
@@ -28,16 +17,12 @@ export default function Home() {
           </p>
         </div>
         <div className="flex flex-col gap-5 justify-normal items-center ">
-          {!!quizzes
-            ? quizzes.map(
-                ({ title, icon }: { title: string; icon: string }) => (
-                  <SelectionButton key={title}>
-                    <Image src={icon} height={20} width={20} alt="title" />
-                    <span>{title}</span>
-                  </SelectionButton>
-                )
-              )
-            : null}
+          {quizzes.map(({ title, icon }: { title: string; icon: string }) => (
+            <SelectionButton key={title}>
+              <Image src={icon} height={20} width={20} alt="title" />
+              <span>{title}</span>
+            </SelectionButton>
+          ))}
         </div>
       </main>
     </>
