de410e986bfae4f1ae13922de119ccda9d50fbf2
muizu <muizuvais99@gmail.com>
2024-05-01 20:39:18 +0530

    added onclick

--- src/components/SelectionButton.tsx
+++ src/components/SelectionButton.tsx
@@ -1,11 +1,16 @@
 "use client";
+
 import React, { ReactNode } from "react";
 type TButton = {
   children: ReactNode;
+  onClick?: () => void;
 };
-export default function SelectionButton({ children }: TButton) {
+export default function SelectionButton({ children, onClick }: TButton) {
   return (
-    <button className="dark:bg-[#3b4d66] bg-[#fff] h-[20px] w-[25vw] z-0 px-5 py-8 flex justify-start items-center gap-5 rounded-lg">
+    <button
+      className="dark:bg-[#3b4d66] bg-[#fff] h-[20px] w-[25vw] z-0 px-5 py-8 flex justify-start items-center gap-5 rounded-lg"
+      onClick={onClick}
+    >
       {children}
     </button>
   );
