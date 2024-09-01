c278bafc8bc7ac160fd3df7b0c2031c8fe4ce592
muizu <muizuvais99@gmail.com>
2024-09-01 16:08:48 +0530

    updated toggle

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -13,7 +13,9 @@ const Toggle = ({ value, onChange }: TToggle) => {
         onChange={onChange}
         className="sr-only peer"
       />
-      <div className="w-11 h-6  peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full after:border-white peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600 bg-primary"></div>
+      <div className="w-10 h-6 flex items-center px-[3px] peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:justify-end peer-checked:bg-blue-600 bg-primary transition-all duration-300">
+        <div className="h-[18px] w-[18px] bg-white rounded-full shadow-md transform transition-transform duration-300"></div>
+      </div>
     </label>
   );
 };
